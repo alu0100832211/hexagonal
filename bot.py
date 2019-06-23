daff02c317a2759dc44497923eb29be8365dc1a4
Martín Belda <alu0100832211@ull.edu.es>
2019-06-23 23:25:22 +0100

    Adds class for Bot

--- bot.py
+++ bot.py
@@ -3,166 +3,65 @@
 import os
 import logging
 import slack
+import asyncio
 import ssl as ssl_lib
 import certifi
 from onboarding_tutorial import OnboardingTutorial, CreateMessage
 from application import Application
 
 
-createmsg = CreateMessage()
-app = Application()
-# For simplicity we'll store our app data in-memory with the following data structure.
-# onboarding_tutorials_sent = {"channel": {"user_id": OnboardingTutorial}}
-onboarding_tutorials_sent = {}
 
+class Bot:
+    def __init__(self, app: Application):
+        self.app = app
 
-def start_onboarding(web_client: slack.WebClient, user_id: str, channel: str):
-    # Create a new onboarding tutorial.
-    onboarding_tutorial = OnboardingTutorial(channel)
+    @slack.RTMClient.run_on(event="message")
+    async def message(self, **payload):
+        """Display the onboarding welcome message after receiving a message
+        that contains "start".
+        """
 
-    # Get the onboarding message payload
-    message = onboarding_tutorial.get_message_payload()
+        data = payload["data"]
+        web_client = payload["web_client"]
+        channel_id = data.get("channel")
 
-    for arg in message:
-        print(f"{arg}={message[arg]}")
+        print(f"Mensaje recibido de channel {channel_id}")
+        user_id = data.get("user")
+        text = data.get("text")
 
+        params = text.split()
 
-    # Post the onboarding message in Slack
-    response = web_client.chat_postMessage(**message)
+        if user_id is not None:
+            web_client.chat_postMessage(channel=channel_id, text=f"Hola <@{user_id}> tu mensaje es {text}")
 
-    # Capture the timestamp of the message we've just posted so
-    # we can use it to update the message after a user
-    # has completed an onboarding task.
-    onboarding_tutorial.timestamp = response["ts"]
-
-    # Store the message sent in onboarding_tutorials_sent
-    if channel not in onboarding_tutorials_sent:
-        onboarding_tutorials_sent[channel] = {}
-    onboarding_tutorials_sent[channel][user_id] = onboarding_tutorial
-
-
-# ================ Team Join Event =============== #
-# When the user first joins a team, the type of the event will be 'team_join'.
-# Here we'll link the onboarding_message callback to the 'team_join' event.
-@slack.RTMClient.run_on(event="team_join")
-def onboarding_message(**payload):
-    """Create and send an onboarding welcome message to new users. Save the
-    time stamp of this message so we can update this message in the future.
-    """
-    # Get WebClient so you can communicate back to Slack.
-    web_client = payload["web_client"]
-
-    # Get the id of the Slack user associated with the incoming event
-    user_id = payload["data"]["user"]["id"]
-
-    # Open a DM with the new user.
-    response = web_client.im_open(user_id)
-    channel = response["channel"]["id"]
-
-    # Post the onboarding message.
-    start_onboarding(web_client, user_id, channel)
-
-
-# ============= Reaction Added Events ============= #
-# When a users adds an emoji reaction to the onboarding message,
-# the type of the event will be 'reaction_added'.
-# Here we'll link the update_emoji callback to the 'reaction_added' event.
-@slack.RTMClient.run_on(event="reaction_added")
-def update_emoji(**payload):
-    """Update the onboarding welcome message after recieving a "reaction_added"
-    event from Slack. Update timestamp for welcome message as well.
-    """
-    data = payload["data"]
-    web_client = payload["web_client"]
-    channel_id = data["item"]["channel"]
-    user_id = data["user"]
-
-    # Get the original tutorial sent.
-    onboarding_tutorial = onboarding_tutorials_sent[channel_id][user_id]
-
-    # Mark the reaction task as completed.
-    onboarding_tutorial.reaction_task_completed = True
-
-    # Get the new message payload
-    message = onboarding_tutorial.get_message_payload()
-
-    # Post the updated message in Slack
-    updated_message = web_client.chat_update(**message)
-
-    # Update the timestamp saved on the onboarding tutorial object
-    onboarding_tutorial.timestamp = updated_message["ts"]
-
-# =============== Pin Added Events ================ #
-# When a users pins a message the type of the event will be 'pin_added'.
-# Here we'll link the update_pin callback to the 'reaction_added' event.
-@slack.RTMClient.run_on(event="pin_added")
-def update_pin(**payload):
-    """Update the onboarding welcome message after recieving a "pin_added"
-    event from Slack. Update timestamp for welcome message as well.
-    """
-    data = payload["data"]
-    web_client = payload["web_client"]
-    channel_id = data["channel_id"]
-    user_id = data["user"]
-
-    # Get the original tutorial sent.
-    onboarding_tutorial = onboarding_tutorials_sent[channel_id][user_id]
-
-    # Mark the pin task as completed.
-    onboarding_tutorial.pin_task_completed = True
-
-    # Get the new message payload
-    message = onboarding_tutorial.get_message_payload()
-
-    # Post the updated message in Slack
-    updated_message = web_client.chat_update(**message)
-
-    # Update the timestamp saved on the onboarding tutorial object
-    onboarding_tutorial.timestamp = updated_message["ts"]
-
-# ============== Message Events ============= #
-# When a user sends a DM, the event type will be 'message'.
-# Here we'll link the message callback to the 'message' event.
-@slack.RTMClient.run_on(event="message")
-def message(**payload):
-    """Display the onboarding welcome message after receiving a message
-    that contains "start".
-    """
-
-    data = payload["data"]
-    web_client = payload["web_client"]
-    channel_id = data.get("channel")
-
-    print(f"Mensaje recibido de channel {channel_id}")
-    user_id = data.get("user")
-    text = data.get("text")
-
-    params = text.split()
-
-    if user_id is not None:
-        web_client.chat_postMessage(channel=channel_id, text=f"Hola <@{user_id}> tu mensaje es {text}")
-
-        if params.pop(0) == "<@UKMCDNT7X>":
-            if params.pop(0) == "list":
-                badges_list=createmsg.badges_list(app.list_badges())
-                web_client.chat_postMessage(channel=channel_id, blocks=badges_list)
+            if params.pop(0) == "<@UKMCDNT7X>":
+                if params.pop(0) == "list":
+                    badges_list=createmsg.badges_list(self.app.list_badges())
+                    web_client.chat_postMessage(channel=channel_id, blocks=badges_list)
 
 #        for idx, param in enumerate(params):
 #            web_client.chat_postMessage(channel=channel_id, text=f"parametro {idx} {param}")
 
-    if text and text.lower() == "start":
-        return start_onboarding(web_client, user_id, channel_id)
-    else:
-        return
+        if text and text.lower() == "start":
+            return start_onboarding(web_client, user_id, channel_id)
+        else:
+            return
 
-def start():
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler())
-    ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
-    slack_token = os.environ["SLACK_BOT_TOKEN"]
-    rtm_client = slack.RTMClient(token=slack_token, ssl=ssl_context)
-    rtm_client.start()
+    def start(self):
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(logging.StreamHandler())
+        ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
+        slack_token = os.environ["SLACK_BOT_TOKEN"]
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        rtm_client = slack.RTMClient(
+                token=slack_token, ssl=ssl_context, run_async=True, loop=loop)
+        loop.run_until_complete(rtm_client.start())
 
 if __name__ == '__main__':
-    start()
+    app = Application()
+
+    bot = Bot(app)
+
+    bot.start()
